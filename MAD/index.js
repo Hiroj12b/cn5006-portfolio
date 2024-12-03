6d4f98ca359ba73911447d1bcce45519963ba0e1
Hiroj12b <hirojbhattarai8@gmail.com>
2024-12-03 14:16:20 +0000

    Creating repo for portfolio

---
+++ MAD/index.js
@@ -0,0 +1,17 @@
+console.log("My nname is iroj")
+//const num1 =12;
+//const num2 =15;
+//var sum1= num1 + num2;
+//let sum= num1 +num2;
+//console.log("The sum of" ,num1,"and" , num2, "is", sum,);
+//const number= parseInt("Enter a number:");
+
+let number = 0;
+number=5;
+if(number > 0){
+    console.log("The number is positive");
+    
+    }
+    else {
+    console.log("The number is negative"); 
+    }
